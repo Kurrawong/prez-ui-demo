e79431f76eac5c22fca694667d8d37a194ee0ee3
hjohns <howiejohns@gmail.com>
2023-12-09 07:25:19 +1100

    update prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -55,7 +55,12 @@ export default defineNuxtConfig({
   css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
   build: {
     transpile: ['vuetify'],
-    
+
+  },
+  nitro: {
+    prerender: {
+      failOnError: false
+    }
   },
   vite: {
     define: {
